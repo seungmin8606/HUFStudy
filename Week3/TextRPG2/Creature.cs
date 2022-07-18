447feecd728b6d3f86404fdf072185edef2e0b5e
Lee SeungMin <91493937+seungmin8606@users.noreply.github.com>
2022-07-18 19:51:25 +0900

    Add files via upload

---
+++ Week3/TextRPG2/Creature.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextRPG2
+{
+	public enum CreatureType
+	{
+		None = 0,
+		Player = 1,
+		Monster = 2
+	}
+
+	class Creature
+	{
+		protected CreatureType type;
+		protected int hp;
+		protected int attack;
+
+		protected Creature(CreatureType type)
+		{
+			this.type = type;
+		}
+
+		public void SetInfo(int hp, int attack)
+		{
+			this.hp = hp;
+			this.attack = attack;
+		}
+
+		public int GetHp() { return hp; }
+		public int GetAttack() { return attack; }
+
+		public bool IsDead() { return hp <= 0; }
+
+		public void OnDamaged(int damage)
+		{
+			hp -= damage;
+			if (hp < 0)
+				hp = 0;
+		}
+	}
+}
---
+++ Week3/TextRPG2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TextRPG2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Game game = new Game();
+
+			while (true)
+			{
+				game.Process();
+			}
+		}
+	}
+}
